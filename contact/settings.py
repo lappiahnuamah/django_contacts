9426d961eee43b58d30d06ba0e91d960ae204409
Lawrence Appiah-Nuamah <44087770+lappiahnuamah@users.noreply.github.com>
2023-08-18 04:10:39 +0000

    Update settings.py

--- contact/settings.py
+++ contact/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3b1tdtnb5*ve)i^5*p#o)d&#jpz@q1!t7qd4$qp$^=+@6u030k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
